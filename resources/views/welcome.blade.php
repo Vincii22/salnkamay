a04f3902f6fdbbfbd6da50e926c1eea618e454d1
Arvin Milan <arvingm1522@gmail.com>
2025-02-19 22:24:21 +0800

    Responsive Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,14 +10,13 @@
 <body class="bg-cover bg-center" style="background-image: url('{{ asset('images/1.png') }}');">
 
 
-    <div class="h-screen flex items-center justify-center text-white flex-col">
+    <div class="h-screen flex items-center justify-center text-white flex-col px-4 sm:px-8">
         <a href="{{ url('/home') }}">
-            <img src="{{ asset('images/2.png') }}" alt="Your Image" class="w-full max-w-[90vw]  rounded-lg transition-all duration-300 hover:scale-110  hover:rounded-full">
+            <!-- Image styling for mobile responsiveness -->
+            <img src="{{ asset('images/2.png') }}" alt="Your Image" class="w-full max-w-[80vw] sm:max-w-[70vw] rounded-lg transition-all duration-300 hover:scale-110 hover:rounded-full">
         </a>
-        <h1 class="text-4xl font-bold mt-4">Welcome to SALINKAMAY!</h1>
+        <h1 class="text-3xl sm:text-4xl md:text-5xl font-bold mt-4">Welcome to SALINKAMAY!</h1>
     </div>
-
-
 
 </body>
 </html>
